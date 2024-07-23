337b57589069fc51b4af7e65350628541002c6e6
MatheusGCruz <matheuskaze@gmail.com>
2024-07-23 09:06:22 -0300

    Firstsend

---
+++ src/main/java/com/weatherMicroservice/EnemSimulado1Application.java
@@ -0,0 +1,13 @@
+package com.weatherMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnemSimulado1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnemSimulado1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/weatherMicroservice/EnemSimulado1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnemSimulado1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
